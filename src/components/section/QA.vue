e324c81d5b01df43bf744dcd4ede1c6c6bc4c4c4
Phongsathorn Kittiworapanya <Phongsathron@outlook.com>
2019-03-15 01:38:59 +0700

    Add QA section

--- src/components/section/QA.vue
+++ src/components/section/QA.vue
@@ -1,5 +1,11 @@
 <template>
-  <div></div>
+  <section class="home-section" id="qa">
+    <div class="strip-top"></div>
+    <div class="strip-bottom"></div>
+    <div class="container">
+      <h1 class="text-center">คำถามที่พบบ่อย</h1>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -9,4 +15,27 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+#qa{
+  padding: 100px 0px;
+  background: #ebebeb;
+}
+#qa .strip-top{
+    background: #ebebeb;
+    width: 100%;
+    height: 50%;
+    position: absolute;
+    top: -45px;
+    transform: skewY(3deg);
+    z-index: 99;
+}
+
+#qa .strip-bottom{
+    background: #ebebeb;
+    width: 100%;
+    height: 50%;
+    position: absolute;
+    bottom: -45px;
+    transform: skewY(-3deg);
+    z-index: 99;
+}
 </style>
