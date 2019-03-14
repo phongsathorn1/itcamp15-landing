c1ba3d05e0ec3440cd0f2512ece0b4b11caee42f
Phongsathorn Kittiworapanya <Phongsathron@outlook.com>
2019-03-15 01:39:07 +0700

    Add gallery section

--- src/components/section/Gallery.vue
+++ src/components/section/Gallery.vue
@@ -1,5 +1,9 @@
 <template>
-  <div></div>
+  <section class="home-section" id="gallery">
+    <div class="container">
+      <h1 class="text-center">ภาพกิจกรรม</h1>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -9,4 +13,12 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+#gallery{
+  background: #fbfbfb;
+  padding: 100px 0px;
+}
+
+#gallery h1{
+  color: #333333;
+}
 </style>
