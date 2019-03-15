da3d66bb68dd2095ac6f0376ea27ee8098b8b165
Phongsathorn Kittiworapanya <Phongsathron@outlook.com>
2019-03-15 15:49:35 +0700

    Update font style

--- src/components/section/QA.vue
+++ src/components/section/QA.vue
@@ -4,6 +4,18 @@
     <div class="strip-bottom"></div>
     <div class="container">
       <h1 class="text-center">คำถามที่พบบ่อย</h1>
+      <div class="qa-wrapper">
+        <ul class="qa-list">
+          <li>
+            <div class="qa-question">
+              ใครสมัครค่ายนี้ได้บ้าง?
+            </div>
+            <div class="qa-answer">
+              <u><b>ตอบ</b></u> น้องๆ ที่<u><b>กำลัง</b></u>ศึกษาในระดับชั้น ม.4 ถึง ม.6 และน้องๆ ที่ศึกษา ปวช. ปวส. หรือเทียบเท่า
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   </section>
 </template>
@@ -20,22 +32,27 @@ export default {
   background: #ebebeb;
 }
 #qa .strip-top{
-    background: #ebebeb;
-    width: 100%;
-    height: 50%;
-    position: absolute;
-    top: -45px;
-    transform: skewY(3deg);
-    z-index: 99;
+  background: #ebebeb;
+  width: 100%;
+  height: 50%;
+  position: absolute;
+  top: -45px;
+  transform: skewY(3deg);
+  z-index: 99;
 }
 
 #qa .strip-bottom{
-    background: #ebebeb;
-    width: 100%;
-    height: 50%;
-    position: absolute;
-    bottom: -45px;
-    transform: skewY(-3deg);
-    z-index: 99;
+  background: #ebebeb;
+  width: 100%;
+  height: 50%;
+  position: absolute;
+  bottom: -45px;
+  transform: skewY(-3deg);
+  z-index: 99;
+}
+
+#qa .container{
+  position: relative;
+  z-index: 100;
 }
 </style>
