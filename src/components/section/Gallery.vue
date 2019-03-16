882d3e8794d78b4a52cda418518a5075663819b7
Wiput Pootong <wiput.pootong@gmail.com>
2019-03-17 02:54:08 +0700

    :ambulance: lazy load

--- src/components/section/Gallery.vue
+++ src/components/section/Gallery.vue
@@ -44,6 +44,7 @@ export default {
         centerPadding: '20px',
         lazyLoad: 'ondemand',
         slidesToShow: 3,
+        slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2000,
         responsive: [
