0162013c88bbe8c10a75c96043aebbde3723af73
Phongsathorn Kittiworapanya <Phongsathron@outlook.com>
2019-03-16 21:35:25 +0700

    Add slider

--- src/components/section/Gallery.vue
+++ src/components/section/Gallery.vue
@@ -1,24 +1,129 @@
 <template>
   <section class="home-section" id="gallery">
-    <div class="container">
+    <div class="container-fluid">
       <h1 class="text-center">ภาพกิจกรรม</h1>
+      <div class="gallery-wrapper">
+        <slick class="gallery-slider" ref="slick" :options="slickOptions">
+          <div class="gallery-item" v-for="image in images" :key="image">
+            <img :src="image" alt>
+          </div>
+        </slick>
+      </div>
     </div>
   </section>
 </template>
 
 <script>
-export default {
+import Slick from 'vue-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
 
+export default {
+  components: {
+    Slick
+  },
+  data () {
+    return {
+      images: [
+        'images/slide/DSC_4556.jpg',
+        'images/slide/IMG_1412.jpg',
+        // 'images/slide/LRM_EXPORT_20180604_090735.jpg',
+        'images/slide/DSC_5869.jpg',
+        'images/slide/IMG_6816.jpg',
+        'images/slide/_DSC6401.jpg',
+        'images/slide/DSC_6534.jpg',
+        'images/slide/IMG_9685.JPG',
+        'images/slide/_DSC8423.jpg',
+        'images/slide/DSC_7206.jpg',
+        'images/slide/IMG_9921.JPG',
+        'images/slide/IMG_1363.jpg',
+        'images/slide/LRM_EXPORT_20180604_090120.jpg'
+      ],
+      slickOptions: {
+        centerMode: true,
+        centerPadding: '20px',
+        slidesToShow: 3,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1
+            }
+          }
+        ]
+      }
+    }
+  },
+  beforeUpdate () {
+    if (this.$refs.slick) {
+      this.$refs.slick.destroy()
+    }
+  },
+  updated () {
+    this.$nextTick(function () {
+      if (this.$refs.slick) {
+        this.$refs.slick.create(this.slickOptions)
+      }
+    })
+  },
+  methods: {
+    next () {
+      this.$refs.slick.next()
+    },
+    prev () {
+      this.$refs.slick.prev()
+    },
+    reInit () {
+      // Helpful if you have to deal with v-for to update dynamic lists
+      this.$nextTick(() => {
+        this.$refs.slick.reSlick()
+      })
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-#gallery{
+#gallery {
   background: #fbfbfb;
   padding: 100px 0px;
 }
 
-#gallery h1{
+#gallery h1 {
+  color: #333333;
+}
+
+.gallery-slider .slick-slide img {
+  width: 100%;
+  display: block;
+}
+
+.gallery-slider .slick-center .gallery-item {
+  transform: scale(1.08);
+}
+
+.gallery-slider .gallery-item {
+  padding: 3%;
+  transition: all 300ms ease;
+}
+
+.gallery-wrapper {
+  padding: 50px 150px;
+}
+
+.slick-prev:before, .slick-next:before{
   color: #333333;
 }
 </style>
